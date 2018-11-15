37938592d3a047399507970cd5c9408901e905cb
mcgregorian1 <mcgregori@si.edu>
2018-11-15 11:09:48 -0500

    Update canopy_position.R

--- Rscripts/canopy_position/canopy_position.R
+++ Rscripts/canopy_position/canopy_position.R
@@ -7,4 +7,15 @@ mortcanopy <- subset(mort18, mort18$crown.position==c("A","CD","CR","D","I","OG"
 library(data.table)
 setnames(mortcanopy, old="StemTag", new="stemtag")
 
-mortcanopy <- mortcanopy[ ,c("tag","stemtag","stemID","sp","dbh.2013","status.2018","dbh.if.dead","perc.crown","crown.position")]                     
+mortcanopy <- mortcanopy[ ,c("tag","stemtag","stemID","sp","dbh.2013","status.2018","dbh.if.dead","perc.crown","crown.position")]             
+
+##merge biannual, survey, and mortality data together
+fullcanopy <- 
+
+## split by dominant/codominant and intermediate/suppressed
+domco <- subset(mortcanopy, mortcanopy$crown.position %in% c("D","CD"))
+intsu <- subset(mortcanopy, mortcanopy$crown.position %in% c("I","S"))
+
+## splt by above 35cm and below 35 cm dbh
+large <- subset(mortcanopy, mortcanopy$dbh.2013 >= 350)
+small <- subset(mortcanopy, mortcanopy$dbh.2013 <350)
