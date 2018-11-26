4786f85f75baa0848c744d6b45d514526af2be10
mcgregorian1 <mcgregori@si.edu>
2018-11-26 09:37:39 -0500

    Update canopy_position.R

--- Rscripts/canopy_position/canopy_position.R
+++ Rscripts/canopy_position/canopy_position.R
@@ -1,5 +1,7 @@
 # canopy positions
 
+## this script is to compare canopy position with trees that are bigger/smaller than 35cm dbh, and to compare dbh by canopy position (dominant/codominant; intermediate/suppressed)
+
 mort18 <- read.csv("C:/Users/mcgregori/Dropbox (Smithsonian)/Github_Ian/SCBI-ForestGEO-Data_private/SCBI_mortality/data/mortality_2018.csv")
 
 mortcanopy <- subset(mort18, mort18$crown.position==c("A","CD","CR","D","I","OG","S"))
@@ -9,7 +11,7 @@ setnames(mortcanopy, old="StemTag", new="stemtag")
 
 mortcanopy <- mortcanopy[ ,c("tag","stemtag","stemID","sp","dbh.2013","status.2018","dbh.if.dead","perc.crown","crown.position")]             
 
-##merge biannual, survey, and mortality data together
+##merge biannual, crown assessment survey, and mortality data together
 fullcanopy <- 
 
 ## split by dominant/codominant and intermediate/suppressed
