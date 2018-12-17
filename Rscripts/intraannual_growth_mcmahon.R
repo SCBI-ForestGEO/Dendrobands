bfc5a5285042f05386b69857155ca672351d6d7f
mcgregorian1 <mcgregori@si.edu>
2018-12-17 10:25:16 -0500

    Created first graphs of tree growth using caliper measurements for 2018

--- Rscripts/intraannual_growth_mcmahon.R
+++ Rscripts/intraannual_growth_mcmahon.R
@@ -12,29 +12,33 @@ intra <- subset(band18, band18$intraannual == 1)
 tagsintra <- c(unique(intra$tag))
 surveys <- c(unique(intra$survey.ID))
 
+intra$doy <- as.Date(intra$doy, format="%m/%d/%Y")
+intra$doy <- factor(intra$doy, ordered=TRUE)
+
 intraannual <- split(intra, f=c(intra$tag))
 
-intra$doy <- as.Date(intra$doy, format="%m/%d/%Y")
+
+
 
 library(ggplot2)
 
-plot_list = list()
+##this code makes graphs for every dendroband individual, with sub-graphs for those trees with multiple stems. The next bit is to do this same graph but be able to show the dendroband measurements but in dbh changes.
+pdf(file = "Dendroband_caliper_growth_2018.pdf")
 for (i in names(intraannual)){
-  
-  pdf(file = "Dendrobands_2018.pdf")
   
   dendro <- intraannual[[i]]
   
-  ggplot(data = dendro) +
-    aes(x = doy, y = measure) +
+  q <- ggplot(data = dendro) +
+    aes(x = doy, y = measure, group=1) +
     geom_line(color = "#0c4c8a") +
-    labs(title = paste0("Dendroband Growth 2018 ", i),
+    labs(title = paste0("Dendroband Growth 2018 ",sep="_", i),
          x = "Date 2018",
          y = "Caliper measurements") +
-    theme_minimal()
-  print(list(dendro))
-  dev.off()
+    theme_minimal() +
+    facet_wrap(c("tag", "stemtag"))
+  print(q)
 }
+dev.off()
 
 
 
@@ -42,7 +46,7 @@ test <- subset(intra, intra$tag == 10671)
 p <- ggplot(test) +
   aes(x = doy, y = measure) +
   geom_line(color = "#0c4c8a") +
-  labs(title = paste0("Dendroband Growth 2018 ", i),
+  labs(title = paste0("Dendroband Growth 2018 ", "_10671"),
        x = "Date 2018",
        y = "Caliper measurements") +
   theme_minimal()
