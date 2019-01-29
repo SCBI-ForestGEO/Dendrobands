2d1e22842e2aa1976fa15cd68cbeedf6b5bf3a01
mcgregorian1 <mcgregori@si.edu>
2019-01-29 08:38:30 -0500

    Updated with new shp locations in github

--- Rscripts/survey_maps.R
+++ Rscripts/survey_maps.R
@@ -22,13 +22,13 @@ library(broom) #for the tidy function
 library(sf) #for mapping
 library(ggthemes) #for removing graticules when making pdf
 
-scbi_plot <- readOGR("V:/SIGEO/GIS_data/20m_grid.shp")
-deer <- readOGR("V:/SIGEO/GIS_data/deer_exclosure_2011.shp")
-roads <- readOGR("V:/SIGEO/GIS_data/SCBI_roads_edits.shp")
-streams <- readOGR("V:/SIGEO/GIS_data/SCBI_streams_edits.shp")
-survey_areas <- readOGR("V:/SIGEO/GIS_data/dendroband surveys/dendroband (bi)annual/biannual_survey_areas.shp")
-NS_divide <- readOGR("V:/SIGEO/GIS_data/dendroband surveys/dendroband intraannual/NS_divide1.shp")
+scbi_plot <- readOGR("C:/Users/mcgregori/Dropbox (Smithsonian)/Github_Ian/SCBI-ForestGEO-Data/spatial_data/20m_grid.shp")
+deer <- readOGR("C:/Users/mcgregori/Dropbox (Smithsonian)/Github_Ian/SCBI-ForestGEO-Data/spatial_data/deer_exclosure_2011.shp")
+roads <- readOGR("C:/Users/mcgregori/Dropbox (Smithsonian)/Github_Ian/SCBI-ForestGEO-Data/spatial_data/SCBI_roads_edits.shp")
+streams <- readOGR("C:/Users/mcgregori/Dropbox (Smithsonian)/Github_Ian/SCBI-ForestGEO-Data/spatial_data/SCBI_streams_edits.shp")
 contour_10m <- readOGR("V:/SIGEO/GIS_data/dendroband surveys/dendroband intraannual/contour10m_SIGEO_clipped.shp")
+survey_areas <- readOGR("C:/Users/mcgregori/Dropbox (Smithsonian)/Github_Ian/Dendrobands/resources/maps/shapefiles/biannual_survey_areas.shp")
+NS_divide <- readOGR("C:/Users/mcgregori/Dropbox (Smithsonian)/Github_Ian/Dendrobands/resources/maps/shapefiles/NS_divide1.shp")
 
 #convert all shp to dataframe so that it can be used by ggplot
 scbi_plot_df <- tidy(scbi_plot)
