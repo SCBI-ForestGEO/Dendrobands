a5d5ec4d15a1c5a306da3dda9c102ca2ddf861f5
mcgregorian1 <mcgregori@si.edu>
2019-02-05 09:01:15 -0500

    Updated dendroID chronologies
    1. Updated scbi.dendroAll_YEAR files with correct stemID and dendroID information.
    
    2. Updated dendro_trees with correct stemID info.
    
    3. Created dendroID.csv to catalogue the chronology of dendroIDs (including information on dendDiam, dendHt, etc. In other words, it's a more comprehensive dataset than "dendroID_chronology.csv")
    
    4. Updated dendroID_chronology.csv (but I believe this can now be moved to archive or deleted, since the creation of "dendroID.csv" has basically rendered it irrelevant).
    
    5. Deleted installation_data.csv (was temp)
    
    6. Created dendroID_chronology.R code that made the "dendroID.csv". This is meant to be used for future dendroID updates as well.

---
+++ Rscripts/analysis/dendroID_chronology.R
@@ -0,0 +1,66 @@
+#script to update the install.csv with new band information#
+
+#1. update install.csv ####
+setwd("C:/Users/mcgregori/Dropbox (Smithsonian)/Github_Ian/Dendrobands/data")
+
+#read in year's data
+dendro2019 <- read.csv("C:/Users/mcgregori/Dropbox (Smithsonian)/Github_Ian/Dendrobands/data/scbi.dendroAll_2019.csv")
+
+install <- read.csv("C:/Users/mcgregori/Dropbox (Smithsonian)/Github_Ian/Dendrobands/data/install.csv")
+
+#subset by new.band=1 and then subset by columns to match install
+dendro2019 <- dendro2019[dendro2019$new.band==1, ]
+
+dendro2019 <- dendro2019[ ,colnames(install) %in% c("tag", "stemtag", "sp", "quadrat", "stemID", "treeID", "biannual", "intraannual", "survey.ID", "dendDiam", "dbh", "new.band", "dendroID", "type", "dir", "dendHt", "crown.condition", "crown.illum", "lianas", "measureID")]
+
+#append the rows to install
+install_new <- rbind(dendro2019, install)
+install_new <- install_new[order(install_new$tag, install_new$stemtag), ]
+
+write.csv(install_new, "dendroID.csv", row.names=FALSE)
+
+############################################################################################
+#2. troubleshooting for all previous years / original code to make install.csv ####
+##this could have been easily done with a for-loop but it was easier doing separate
+
+dendro2010 <- read.csv("C:/Users/mcgregori/Dropbox (Smithsonian)/Github_Ian/Dendrobands/data/scbi.dendroAll_2010.csv")
+
+dendro2011 <- read.csv("C:/Users/mcgregori/Dropbox (Smithsonian)/Github_Ian/Dendrobands/data/scbi.dendroAll_2011.csv")
+
+dendro2012 <- read.csv("C:/Users/mcgregori/Dropbox (Smithsonian)/Github_Ian/Dendrobands/data/scbi.dendroAll_2012.csv")
+
+dendro2013 <- read.csv("C:/Users/mcgregori/Dropbox (Smithsonian)/Github_Ian/Dendrobands/data/scbi.dendroAll_2013.csv")
+
+dendro2014 <- read.csv("C:/Users/mcgregori/Dropbox (Smithsonian)/Github_Ian/Dendrobands/data/scbi.dendroAll_2014.csv")
+
+dendro2015 <- read.csv("C:/Users/mcgregori/Dropbox (Smithsonian)/Github_Ian/Dendrobands/data/scbi.dendroAll_2015.csv")
+
+dendro2016 <- read.csv("C:/Users/mcgregori/Dropbox (Smithsonian)/Github_Ian/Dendrobands/data/scbi.dendroAll_2016.csv")
+
+dendro2017 <- read.csv("C:/Users/mcgregori/Dropbox (Smithsonian)/Github_Ian/Dendrobands/data/scbi.dendroAll_2017.csv")
+
+dendro2018 <- read.csv("C:/Users/mcgregori/Dropbox (Smithsonian)/Github_Ian/Dendrobands/data/scbi.dendroAll_2018.csv")
+
+dendro2011 <- dendro2011[dendro2011$new.band==1, ]
+dendro2012 <- dendro2012[dendro2012$new.band==1, ]
+dendro2013 <- dendro2013[dendro2013$new.band==1, ]
+dendro2014 <- dendro2014[dendro2014$new.band==1, ]
+dendro2015 <- dendro2015[dendro2015$new.band==1, ]
+dendro2016 <- dendro2016[dendro2016$new.band==1, ]
+dendro2017 <- dendro2017[dendro2017$new.band==1, ]
+dendro2018 <- dendro2018[dendro2018$new.band==1, ]
+dendro2018 <- dendro2018[ , -17]
+
+install <- rbind(dendro2010, dendro2011, dendro2012, dendro2013, dendro2014, dendro2015, dendro2016, dendro2017, dendro2018)
+
+install <- install[ ,colnames(install) %in% c("tag", "stemtag", "sp", "quadrat", "stemID", "treeID", "biannual", "intraannual", "survey.ID", "dendDiam", "dbh", "new.band", "dendroID", "type", "dir", "dendHt", "crown.condition", "crown.illum", "lianas", "measureID")]
+
+install <- install[order(install$tag, install$stemtag), ]
+
+dendro_trees <- read.csv("C:/Users/mcgregori/Dropbox (Smithsonian)/Github_Ian/Dendrobands/data/dendro_trees.csv")
+
+length(unique(install$stemID))
+length(unique(dendro_trees$stemID))
+setdiff(dendro_trees$stemID, install$stemID)
+
+write.csv(install, "dendroID.csv", row.names=FALSE)
