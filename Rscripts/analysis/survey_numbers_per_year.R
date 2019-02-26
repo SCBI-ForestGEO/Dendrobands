8105ca93618ad467c624b41b202aa1e491a02656
mcgregorian1 <mcgregori@si.edu>
2019-02-26 16:41:04 -0500

    Added script for calculating survey numbers per year

---
+++ Rscripts/analysis/survey_numbers_per_year.R
@@ -0,0 +1,104 @@
+# dendroband survey numbers per year
+
+setwd("C:/Users/mcgregori/Dropbox (Smithsonian)/Github_Ian/Dendrobands/data")
+dirs <- dir("C:/Users/mcgregori/Dropbox (Smithsonian)/Github_Ian/Dendrobands/data", pattern="_201[0-8]*.csv")
+
+dendro_trees <- read.csv("C:/Users/mcgregori/Dropbox (Smithsonian)/Github_Ian/Dendrobands/data/dendro_trees.csv", stringsAsFactors=FALSE)
+
+survey <- read.csv("C:/Users/mcgregori/Dropbox (Smithsonian)/Github_Ian/Dendrobands/data/survey_numbers_per_year.csv")
+
+for (k in seq(along=dirs)){
+  path <- dirs[[k]]
+  file <- read.csv(path, stringsAsFactors = FALSE)
+  
+  for (i in seq(along=survey$year[1:9])){
+    year <- survey$year[[i]]
+    
+    if (k==i){
+      #n.surveys
+      survey$n.surveys <- ifelse(survey$year %in% survey$year[[i]], length(unique(file$survey.ID)), survey$n.surveys)
+      
+      #__.all
+      all_bi <- dendro_trees[!dendro_trees$dendro.start.year > year, ]
+      survey$biannual.all <- ifelse(survey$year %in% survey$year[[i]], length(unique(all_bi$stemID)), survey$biannual.all)
+      
+      all_intra <- all_bi[all_bi$intraannual == 1, ]
+      survey$intraannual.all <- ifelse(survey$year %in% survey$year[[i]], length(unique(all_intra$stemID)), survey$intraannual.all)
+      
+      #___.live
+      #the min is here because we're interested in seeing the survey numbers as they were at when the growing season started. The only reason to use the max survey.ID is for calculating dead trees and additions.
+      if (year == 2013){
+        alive <- file[file$survey.ID == max(file$survey.ID), ] 
+        #because 2013 new plants added in middle of season
+      } else {
+        alive <- file[file$survey.ID == min(file$survey.ID), ]
+      }
+
+      survey$biannual.live <- ifelse(survey$year %in% survey$year[[i]], length(unique(alive$stemID)), survey$biannual.live)
+      
+      if (year == 2013){
+        alive_intra <- file[file$intraannual == 1 & file$survey.ID == max(file$survey.ID), ] 
+        #because 2013 new plants added in middle of season
+      } else {
+        alive_intra <- file[file$intraannual == 1 & file$survey.ID == min(file$survey.ID), ]
+      }
+      
+      survey$intraannual.live <- ifelse(survey$year %in% survey$year[[i]], length(unique(alive_intra$stemID)), survey$intraannual.live)
+      
+      #___.dead
+      dead <- file[file$status == "dead" & file$survey.ID == max(file$survey.ID), ]
+      survey$biannual.dead <- ifelse(survey$year %in% survey$year[[i]], length(unique(dead$stemID)), survey$biannual.dead)
+      
+      dead_intra <- file[file$status == "dead" & file$survey.ID == max(file$survey.ID) & file$intraannual == 1, ]
+      survey$intraannual.dead <- ifelse(survey$year %in% survey$year[[i]], length(unique(dead_intra$stemID)), survey$intraannual.dead)
+      
+      #___.added
+      new <- dendro_trees[dendro_trees$dendro.start.year %in% survey$year[[i]], ]
+      survey$biannual.added <- ifelse(survey$year %in% survey$year[[i]],length(unique(new$stemID)), survey$biannual.added)
+      
+      new_intra <- dendro_trees[dendro_trees$dendro.start.year %in% survey$year[[i]] & dendro_trees$intraannual == 1, ]
+      survey$intraannual.added <- ifelse(survey$year %in% survey$year[[i]],length(unique(new_intra$stemID)), survey$intraannual.added)
+      
+    }
+  }
+}
+    
+    
+
+########################################
+dendro_2018 <- read.csv("C:/Users/mcgregori/Dropbox (Smithsonian)/Github_Ian/Dendrobands/data/scbi.dendroAll_2018.csv")
+for (i in seq(along=survey$year)){
+  
+  #n.surveys
+  survey$n.surveys <- ifelse(survey$year %in% survey$year[[i]], length(unique(dendro_2018$survey.ID)), survey$n.surveys)
+  
+  #__.all
+  all_bi <- dendro_trees[!dendro_trees$dendro.start.year > 2018, ]
+  survey$biannual.all <- ifelse(survey$year %in% survey$year[[i]], length(unique(all_bi$stemID)), survey$biannual.all)
+
+  all_intra <- all_bi[all_bi$intraannual == 1, ]
+  survey$intraannual.all <- ifelse(survey$year %in% survey$year[[i]], length(unique(all_intra$stemID)), survey$intraannual.all)
+  
+  #___.live
+  #the min is here because we're interested in seeing the survey numbers as they were at when the growing season started. The only reason to use the max survey.ID is for calculating dead trees and additions.
+  alive <- dendro_2018[dendro_2018$status == "alive" & dendro_2018$survey.ID == min(dendro_2018$survey.ID), ]
+  survey$biannual.live <- ifelse(survey$year %in% survey$year[[i]], length(unique(alive$stemID)), survey$biannual.live)
+  
+  alive_intra <- intra[intra$status == "alive" & intra$survey.ID == min(intra$survey.ID), ]
+  survey$intraannual.live <- ifelse(survey$year %in% survey$year[[i]], length(unique(alive_intra$stemID)), survey$intraannual.live)
+  
+  #___.dead
+  dead <- dendro_2018[dendro_2018$status == "dead" & dendro_2018$survey.ID == max(dendro_2018$survey.ID), ]
+  survey$biannual.dead <- ifelse(survey$year %in% survey$year[[i]], length(unique(dead$stemID)), survey$biannual.dead)
+  
+  dead_intra <- dendro_2018[dendro_2018$status == "dead" & dendro_2018$survey.ID == max(dendro_2018$survey.ID) & dendro_2018$intraannual == 1, ]
+  survey$intraannual.dead <- ifelse(survey$year %in% survey$year[[i]], length(unique(dead_intra$stemID)), survey$intraannual.dead)
+  
+  #___.added
+  new <- dendro_trees[dendro_trees$dendro.start.year %in% survey$year[[i]], ]
+  survey$biannual.added <- ifelse(survey$year %in% survey$year[[i]],length(unique(new$stemID)), survey$biannual.added)
+  
+  new_intra <- dendro_trees[dendro_trees$dendro.start.year %in% survey$year[[i]] & dendro_trees$intraannual == 1, ]
+  survey$intraannual.added <- ifelse(survey$year %in% survey$year[[i]],length(unique(new_intra$stemID)), survey$intraannual.added)
+  
+}
