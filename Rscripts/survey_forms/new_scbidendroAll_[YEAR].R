6507e2698ca0d8be807dea27178810710ea3e898
mcgregorian1 <mcgregori@si.edu>
2019-04-02 10:09:49 -0400

    Update new_scbidendroAll_[YEAR].R

--- Rscripts/survey_forms/new_scbidendroAll_[YEAR].R
+++ Rscripts/survey_forms/new_scbidendroAll_[YEAR].R
@@ -10,6 +10,8 @@ data_2019 <- subset(data_2018,survey.ID=="2018.14" & status=="alive")
 cols <- c("survey.ID", "year", "month", "day", "measure", "codes", "notes", "status", "field.recorders", "data.enter", "new.band")
 
 data_2019[,cols] <- ""
+data_2019$crown.condition <- NA
+data_2019$crown.illum <- NA
 
 write.csv(data_2019, "scbi.dendroAll_2019.csv", row.names=FALSE)
 
