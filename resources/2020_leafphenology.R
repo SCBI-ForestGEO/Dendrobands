4b017a68d06339fc19a63a74fdddd84962e8b118
Erika Gonzalez-Akre <gonzalezeb@si.edu>
2020-05-06 16:44:59 -0400

    Update 2020_leafphenology.R

--- resources/2020_leafphenology.R
+++ resources/2020_leafphenology.R
@@ -1,30 +1,45 @@
+## Cameron Dow, MAy 6, 2020
+## Preliminary script to analyze phenology data from dendrobanded trees
+
 library(readr)
 library(dplyr)
-intra02<- read_csv("Dendrobands/resources/data_entry_forms/2020/data_entry_intraannual_2020-02.csv")
-intra03 <- read_csv("Dendrobands/resources/data_entry_forms/2020/data_entry_intraannual_2020-03.csv")
-intra04 <- read_csv("Dendrobands/resources/data_entry_forms/2020/data_entry_intraannual_2020-04.csv")
 
+#read data files directly from Github (ignore errors for now)
+
+intra02<- read_csv("https://raw.githubusercontent.com/SCBI-ForestGEO/Dendrobands/master/resources/data_entry_forms/2020/data_entry_intraannual_2020-02.csv?token=AGNAWNF6UOUA444C26GHK4C6XRIHM")
+
+intra03 <- read_csv("https://raw.githubusercontent.com/SCBI-ForestGEO/Dendrobands/master/resources/data_entry_forms/2020/data_entry_intraannual_2020-03.csv?token=AGNAWNAEFWIKIO3XYTQTOVK6XRMWU")
+
+intra04 <- read_csv("https://raw.githubusercontent.com/SCBI-ForestGEO/Dendrobands/master/resources/data_entry_forms/2020/data_entry_intraannual_2020-04.csv?token=AGNAWNFSVMFQNGISPL7DZB26XRM72")
+
+# add an column to obtain full dates
 intra02$date <- paste(intra02$month, intra02$day, intra02$year, sep = "/")
 intra03$date <- paste(intra03$month, intra03$day, intra03$year, sep = "/")
 intra04$date <- paste(intra04$month, intra04$day, intra04$year, sep = "/")
 
+#subset to get only pheno data
 survey02 <- intra02[,c(1,2,15)]
 survey03 <- intra03[,c(1,2,15)]
 survey04 <- intra04[,c(1,2,15)]
 
-colnames(survey02) <- c("Tag","stemID" , unique(intra02$date))
-colnames(survey03) <- c("Tag", "stemID" , unique(intra03$date))
-colnames(survey04) <- c("Tag","stemID" , unique(intra04$date))
+#rename columns (don't use "stemID" as that has another meaning)
+colnames(survey02) <- c("Tag","stem" , unique(intra02$date))
+colnames(survey03) <- c("Tag", "stem" , unique(intra03$date))
+colnames(survey04) <- c("Tag","stem" , unique(intra04$date))
+
+#now combine all surveys to get a singular phenology dataset 
 
 phenology2020 <- cbind(survey02,survey03[,3],survey04[,3])
 
-#Combine 2014 DF w/ 2020 DF#
+#Combine with data from 2014#
 
-phenology2014 <- read_csv("SCBI-ForestGEO-Data/leaf phenology/data/SCBI_spring_phenology_2014.csv")
+phenology2014 <- read_csv("https://raw.githubusercontent.com/SCBI-ForestGEO/SCBI-ForestGEO-Data/master/leaf%20phenology/data/SCBI_spring_phenology_2014.csv")
 
+#subset to obtain a dataframe with unique tags
 same_tags <- phenology2020 %>%
   filter(Tag %in% phenology2014$Tag)
 
+#now merge both years in a single data frame
 Pheno_compare <- merge(phenology2014,phenology2020, all=FALSE)
 
 #library(reshape2)
