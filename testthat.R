f5da82c264dbbcfcea7cff3a34881f4cf675b2b1
rudeboybert <albert.ys.kim@gmail.com>
2021-05-17 16:11:35 -0400

    Add documentation

--- testthat.R
+++ testthat.R
@@ -1,3 +1,4 @@
+# See testthat/README.md for more details
 library(testthat)
 
 test_dir("testthat", reporter = c("check", "progress"))
--- testthat/test-dates.R
+++ testthat/test-dates.R
@@ -9,6 +9,7 @@ test_that("All years are valid", {
     str_sub(1, 4) %>% 
     as.numeric()
   
+  # For all .csv's, test that year is between 2010-current year and not NA
   dendroband_measurements <- here("data") %>% 
     dir(path = ., pattern = "scbi.dendroAll*", full.names = TRUE) %>%
     map_dfr(.f = read_csv, col_types = cols(dbh = col_double(), dendDiam = col_double())) %>% 
@@ -18,6 +19,7 @@ test_that("All years are valid", {
     pull(year_valid) %>% 
     all() 
   
+  # Write report if any errors
   if(!all_years_valid){
     filename <- here("testthat/reports", str_c(Sys.Date(), "_year_issues.csv"))
     
