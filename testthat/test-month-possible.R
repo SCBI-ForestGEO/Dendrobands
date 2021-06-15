7c0ad22e1786d202fb248f8dffa6e1d9bb064bb3
rudeboybert <albert.ys.kim@gmail.com>
2021-06-15 09:35:47 -0400

    remove date from error report

--- testthat/test-month-possible.R
+++ testthat/test-month-possible.R
@@ -15,13 +15,13 @@ test_that("Month is possible", {
   dendroband_measurements <- dendroband_measurements %>% 
     mutate(month_possible = between(month, 1, 12) & !is.na(month)) 
   
-  # Test if any errors
+  # Test if all months are possible
   all_months_possible <- dendroband_measurements %>% 
     pull(month_possible) %>% 
     all() 
   
   # If any errors, write report. Otherwise, delete any existing reports
-  filename <- here("testthat/reports", str_c(Sys.Date(), "_month_possible.csv"))
+  filename <- here("testthat/reports/month_possible.csv")
   
   if(!all_months_possible){
     dendroband_measurements %>% 
