43710f42c4dff457ed7200ccc82468153b71780a
rudeboybert <albert.ys.kim@gmail.com>
2021-06-15 08:55:31 -0400

    rename year-possible

--- testthat/test-year-possible.R
+++ testthat/test-year-possible.R
@@ -4,22 +4,25 @@ library(readr)
 library(stringr)
 library(purrr)
 
-test_that("All years are valid", {
+test_that("Year is possible", {
+  # Get current year
   current_year <- Sys.Date() %>% 
     str_sub(1, 4) %>% 
     as.numeric()
   
-  # For all .csv's, test that year is between 2010-current year and not NA
-  dendroband_measurements <- here("data") %>% 
+  dendroband_measurements <- 
+    # Load all csv's at once
+    here("data") %>% 
     dir(path = ., pattern = "scbi.dendroAll*", full.names = TRUE) %>%
     map_dfr(.f = read_csv, col_types = cols(dbh = col_double(), dendDiam = col_double())) %>% 
+    # Test that year is between 2010-current year and not NA
     mutate(year_valid = between(year, 2010, current_year) & !is.na(year)) 
   
+  # Test & write report if any errors
   all_years_valid <- dendroband_measurements %>% 
     pull(year_valid) %>% 
     all() 
   
-  # Write report if any errors
   if(!all_years_valid){
     filename <- here("testthat/reports", str_c(Sys.Date(), "_year_issues.csv"))
     
