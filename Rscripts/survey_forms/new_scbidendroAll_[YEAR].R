1be06cec32367b7f275fa5930996dec8c05b8754
rudeboybert <albert.ys.kim@gmail.com>
2021-07-02 09:29:16 -0400

    Updated year to be set dynamically

--- Rscripts/survey_forms/new_scbidendroAll_[YEAR].R
+++ Rscripts/survey_forms/new_scbidendroAll_[YEAR].R
@@ -1,21 +1,36 @@
 ######################################################
-# Purpose: Create new year's master file (scbi.dendroAll_[YEAR].csv)
+# Purpose: Create new year's master file (data/scbi.dendroAll_[YEAR].csv)
+#
 # Developed by: Ian McGregor - mcgregori@si.edu
 # R version 3.5.2 - First created October 2018
+#
+# Modified by: Albert Y. Kim - albert.ys.kim@gmail.com
+# R version 4.0.3 - Modified July 2021
 ######################################################
+library(dplyr)
+library(stringr)
 
+current_year <- substr(Sys.Date(), 1, 4)
+previous_year <- current_year %>% 
+  as.numeric() %>% 
+  `-`(1) %>% 
+  as.character()
 
-data_2020 <- read.csv("data/scbi.dendroAll_2020.csv")
+# Get previous year's data
+previous_year_data <- 
+  str_c("data/scbi.dendroAll_", previous_year, ".csv") %>% 
+  read.csv()
 
-#subset by the most recent survey and live trees
-data_2021 <- subset(data_2020,survey.ID=="2020.06" & status=="alive")
+# subset by the most recent survey and live trees
+current_year_data <- subset(previous_year_data, survey.ID == max(survey.ID) & status=="alive")
 
 cols <- c("survey.ID", "year", "month", "day", "measure", "codes", "notes", "status", "field.recorders", "data.enter", "new.band")
 
-data_2021[,cols] <- ""
-data_2021$crown.condition <- NA
-data_2021$crown.illum <- NA
+current_year_data[,cols] <- ""
+current_year_data$crown.condition <- NA
+current_year_data$crown.illum <- NA
 
-write.csv(data_2021, "data/scbi.dendroAll_2021.csv", row.names=FALSE)
+str_c("data/scbi.dendroAll_", current_year, ".csv") %>% 
+  write.csv(x = current_year_data, file = ., row.names=FALSE)
 
 
