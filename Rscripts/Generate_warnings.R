51a269f3f954ce504bebc2f0d3f00d8b071c1f50
rudeboybert <albert.ys.kim@gmail.com>
2021-07-20 14:36:06 -0400

    Fix generate warnings script

--- Rscripts/Generate_warnings.R
+++ Rscripts/Generate_warnings.R
@@ -24,7 +24,7 @@ if(file.exists(warning_file_path)) warning_file <- read.csv(warning_file_path)
 
 # write warning messages ####
 
-warning_messages <- c("dendro_band_needs_fixing_or_replacing" = "There are dendrobands that need replacing.",
+warning_messages <- c("dendroband_needs_fixing_or_replacing" = "There are dendrobands that need replacing.",
                       "new_measure_too_different_from_previous" = "There are measures that are suspiciously different from previous ones.")
 
 # check if files exist and generate a plot with the warning ####
@@ -35,7 +35,11 @@ warning_messages <- c("dendro_band_needs_fixing_or_replacing" = "There are dendr
 # if(exists("will_auto_fix_error_file")) all_will_be_fixed <- paste(c("ERRORS THAT WILL AUTO FIX:\n", warning_messages[unique(will_auto_fix_error_file$error_name)]), collapse = "\n") else  all_will_be_fixed <- ""
 
 
-if(exists("warning_file")) all_warns <- paste(c("WARNINGS!!!\n", warning_messages[unique(warning_file$warning_name)], "CLICK HERE TO GO TO FOLDER"), collapse = "\n") else all_warns = "No WARNINGS"
+if(file.exists(warning_file_path)){
+  all_warns <- paste(c("WARNINGS!!!\n", warning_messages[unique(warning_file$alert_name)], "\nCLICK HERE TO GO TO FOLDER"), collapse = "\n") 
+} else{
+  all_warns = "No WARNINGS"
+}
 
 
 filename <- file.path(here("testthat"), "reports/warnings.png")
@@ -51,7 +55,6 @@ if(length(all_warns) == 0)  file.remove(filename)
 #
 # png(filename, width = 6, height = 0.7 + (0.2*length(c(gregexpr("\\n", all_warns)[[1]], gregexpr("\\n", all_will_be_fixed)[[1]]))), units = "in", res = 300)
 png(filename, width = 6, height = 0.7 + (0.2*length(c(gregexpr("\\n", all_warns)[[1]]))), units = "in", res = 300)
-
 par(mar = c(0,0,0,0))
 plot(0,0, axes = F, xlab = "", ylab = "", type = "n")
 text(0,0.7, all_warns, col = "red", cex = 0.6)
