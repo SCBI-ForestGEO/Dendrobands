679d38a96cc0f30df0f3e1cfea5f2f15fa20c314
rudeboybert <albert.ys.kim@gmail.com>
2021-08-23 10:29:30 -0400

    Fixed bug in anomaly visualizations

--- Rscripts/Generate_measurement_anomalies_visualization.R
+++ Rscripts/Generate_measurement_anomalies_visualization.R
@@ -1,6 +1,25 @@
 # Creates visualizations of all new measurements that are "anomalous"
 
 library(ggplot2)
+library(dplyr)
+library(readr)
+library(here)
+library(lubridate)
+library(stringr)
+
+## Load all master data files into a single data frame 
+master_data_filenames <- dir(path = here("data"), pattern = "scbi.dendroAll*", full.names = TRUE)
+
+dendroband_measurements <- NULL
+for(i in 1:length(master_data_filenames)){
+  dendroband_measurements <- 
+    bind_rows(
+      dendroband_measurements,
+      read_csv(master_data_filenames[i], col_types = cols(dbh = col_double(), dendDiam = col_double()))
+    )
+}
+
+
 
 
 anomalies <- read_csv("testthat/reports/warnings/warnings_file.csv") %>% 
