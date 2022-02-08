9ddebb8ac4e43b391f75c6ad668e64d26129d787
rudeboybert <albert.ys.kim@gmail.com>
2022-02-08 10:25:50 -0500

    check if current year fall biannual survey has been done

--- Rscripts/analysis/dendrotrees_by_sp-survey-dbh.R
+++ Rscripts/analysis/dendrotrees_by_sp-survey-dbh.R
@@ -15,28 +15,41 @@ library(here)
 library(data.table)
 library(purrr)
 
+# Get current year
 current_year <- Sys.Date() %>% 
   year()
 
-for(current_year in c(2010:2021)){
-  current_year_data <-
-    str_c("data/scbi.dendroAll_", current_year, ".csv") %>% 
+# If fall biannual survey has been conducted for current year, create
+# summary CSV for current year, otherwise don't
+fall_biannual_survey <- str_c("resources/raw_data/", current_year, "/data_entry_biannual_fall", current_year, ".csv") %>% 
+  here()
+if(file.exists(fall_biannual_survey)){
+  max_year <- current_year
+} else {
+  max_year <- current_year -1 
+}
+
+
+for(year in c(2010:2021)){
+  year_data <-
+    str_c("data/scbi.dendroAll_", year, ".csv") %>% 
     read.csv()
-  current_year_spring_survey_ID <- current_year_data$survey.ID %>% 
+  year_spring_survey_ID <- year_data$survey.ID %>% 
     min() %>% 
     as.character()
-  current_year_fall_survey_ID <- current_year_data$survey.ID %>% 
+  year_fall_survey_ID <- year_data$survey.ID %>% 
     max() %>% 
     as.character()
   output_csv_file <- "results/dendro_trees_dbhcount/" %>% 
     here() %>% 
-    str_c("dendro_trees_sp_", current_year, "_min_max_mean_dbh.csv")
+    str_c("dendro_trees_sp_", year, "_min_max_mean_dbh.csv")
   
   
   # subset by the last survey from the year
-  data_surveys <- subset(current_year_data, current_year_data$survey.ID == current_year_fall_survey_ID) # & current_year_data$status=='alive') #get rid of '0' values for minimum
+  data_surveys <- subset(year_data, year_data$survey.ID == year_fall_survey_ID) 
+  # & year_data$status=='alive') #get rid of '0' values for minimum
   
-  data_trees <- current_year_data[!duplicated(current_year_data["sp"]), ]
+  data_trees <- year_data[!duplicated(year_data["sp"]), ]
   data_trees <- data_trees[c("sp")] # list that shows all sp alive and dead
   
   # make data.frames with the dbhmax,min,mean by sp
@@ -55,7 +68,7 @@ for(current_year in c(2010:2021)){
   # create data.frame with count of sp per survey
   
   
-  data_01 <- subset(current_year_data, current_year_data$survey.ID == current_year_spring_survey_ID) # get all sp
+  data_01 <- subset(year_data, year_data$survey.ID == year_spring_survey_ID) # get all sp
   
   countbi <- addmargins(table(data_01$sp, data_01$biannual), 1)
   countbi <- as.data.frame.matrix(countbi)
@@ -78,7 +91,8 @@ for(current_year in c(2010:2021)){
   
   data_merged <- data_merged[order(data_merged$sp), ]
   
-  ## reorder to make "Sum" row be last. These numbers may change depending on if a tree has been fully removed from the survey
+  ## reorder to make "Sum" row be last. These numbers may change
+  ## depending on if a tree has been fully removed from the survey
   data_merged <- data_merged[c(2:20, 22, 23, 21, 1), ]
   
   write.csv(data_merged, output_csv_file, row.names = FALSE)
