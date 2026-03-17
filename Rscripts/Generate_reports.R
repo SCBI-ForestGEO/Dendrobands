9005bd0c8e3d4f010607b2bff6c7ecef8cf0da5b
Valentine Herrmann <herrmannv@si.edu>
2026-03-17 13:18:10 -0400

    trying to fix the GitHub actions

--- Rscripts/Generate_reports.R
+++ Rscripts/Generate_reports.R
@@ -32,7 +32,7 @@ for(i in 1:length(master_data_filenames)){
   dendroband_measurements_all_years <- 
     bind_rows(
       dendroband_measurements_all_years,
-      read_csv(master_data_filenames[i], col_types = cols(dbh = col_double(), dendDiam = col_double(), measure = col_double()))
+      read_csv(master_data_filenames[i], col_types = cols(tag = col_character(), dbh = col_double(), dendDiam = col_double(), measure = col_double()))
     )
 }
 
